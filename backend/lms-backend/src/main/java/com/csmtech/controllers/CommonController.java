419396a374876f1f88ca37cae67f2f4929524280
Chinmaya Jena <chinmaya.jena@corp.csmpl.com>
2024-02-27 21:40:08 +0530

    view file work

--- backend/lms-backend/src/main/java/com/csmtech/controllers/CommonController.java
+++ backend/lms-backend/src/main/java/com/csmtech/controllers/CommonController.java
@@ -2,6 +2,8 @@ package com.csmtech.controllers;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.SecureRandom;
@@ -9,9 +11,16 @@ import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +30,8 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin("*")
 public class CommonController {
 
+	@Value("${actualfile.path}")
+	private String actualFilePath;
 	
 	@Value("${tempfile.path}")
 	private String tempPath;
@@ -65,4 +76,38 @@ public class CommonController {
 		return ResponseEntity.ok().body(response);
 
 	}
+	
+	@GetMapping(path="/viewFile/{fileName}")
+	public ResponseEntity<?> downloadDocument(HttpServletResponse response,@PathVariable("fileName") String fileName) throws FileNotFoundException{
+	 
+	String filePath="";
+	filePath = actualFilePath;
+	HttpHeaders headers = new HttpHeaders();
+	headers.add("content-disposition","inline;filename=" + fileName);
+	File file =new File(filePath +"/" +fileName);
+	InputStreamResource resource=new InputStreamResource(new FileInputStream(file));
+	String contentType = "";
+	if (null != fileName && fileName.contains(".")) {
+	String fileExtension = fileName.split("\\.")[1];
+	if (fileExtension.equalsIgnoreCase("pdf"))
+	contentType = "application/pdf";
+	if (fileExtension.equalsIgnoreCase("xls"))
+	contentType = "application/vnd.ms-excel";
+	if (fileExtension.equalsIgnoreCase("xlsx"))
+	contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+	else if (fileExtension.equalsIgnoreCase("png"))
+	contentType = "image/png";
+	else if (fileExtension.equalsIgnoreCase("jpg") || fileExtension.equalsIgnoreCase("jpeg"))
+	contentType = "image/jpeg";
+	    else if (fileExtension.equalsIgnoreCase("ppt") || fileExtension.equalsIgnoreCase("pptx")) 
+	        contentType = "application/vnd.ms-powerpoint";
+	    else if (fileExtension.equalsIgnoreCase("mp4") || fileExtension.equalsIgnoreCase("avi")) 
+	        contentType = "video/mp4"; // Adjust as needed based on the video format
+	    
+	}
+	 
+	 
+	return ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(MediaType.parseMediaType(contentType)).body(resource);
+	}
+
 }
