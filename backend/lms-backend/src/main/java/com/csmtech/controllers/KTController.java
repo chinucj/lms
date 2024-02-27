5bfa8d5ed480f35e44d259e33a2ab050043023d0
Chinmaya Jena <chinmaya.jena@corp.csmpl.com>
2024-02-27 16:25:43 +0530

    File upload work

--- backend/lms-backend/src/main/java/com/csmtech/controllers/KTController.java
+++ backend/lms-backend/src/main/java/com/csmtech/controllers/KTController.java
@@ -27,71 +27,84 @@ import com.csmtech.services.KTMasterService;
 @RestController
 @CrossOrigin("*")
 public class KTController {
-	
+
 	@Value("${tempfile.path}")
 	private String tempFilePath;
-	 
+
 	@Value("${actualfile.path}")
 	private String actualFilePath;
-	
+
 	@Autowired
 	private KTMasterService ktMasterService;
-	 
 
 	@PostMapping("/kt")
-	public ResponseEntity<KTMaster> saveKTMaster(@RequestBody KTMasterDto ktMasterDto ){
-	KTMaster ktMaster =ktMasterService.saveKTMaster(ktMasterDto);
-	 
-	//for re;[move the file temppath to actualpath and delete the file from temppath
-	 
-	List<String> fileUploadList=new ArrayList<>();
-	fileUploadList.add(ktMasterDto.getKtFilePath());
-	for(String fileUpload:fileUploadList) {
-	if(fileUpload !=null && (!fileUpload.equals(""))) {
-	File file=new File(tempFilePath + fileUpload);
-	if(file.exists()) {
-	File srcFile=new File(tempFilePath + fileUpload);
-	File destFile=new File(actualFilePath + fileUpload);
-	try {
-	Files.copy(srcFile.toPath(), destFile.toPath(),StandardCopyOption.REPLACE_EXISTING);
-	Files.delete(srcFile.toPath());
-	}catch(IOException e) {
-	e.printStackTrace();
+	public ResponseEntity<KTMaster> saveKTMaster(@RequestBody KTMasterDto ktMasterDto) throws Exception {
+		KTMaster ktMaster = ktMasterService.saveKTMaster(ktMasterDto);
+
+		// for re;[move the file temppath to actualpath and delete the file from
+		// temppath
+
+		List<String> fileUploadList = new ArrayList<>();
+		String fileFormat = null;
+		String folderName = null;
+		fileUploadList.add(ktMasterDto.getKtFilePath());
+		for (String fileUpload : fileUploadList) {
+			if (fileUpload != null && (!fileUpload.equals(""))) {
+				int lastDotIndex = fileUpload.lastIndexOf('.');
+				if (lastDotIndex != -1) {
+				    fileFormat = fileUpload.substring(lastDotIndex + 1);
+				    System.out.println(fileFormat);
+				} else {
+				    throw new Exception("No file format found");
+				}
+				if(fileFormat.equals("mp4") || fileFormat.equals("avi") || 
+						fileFormat.equals("wmv") || fileFormat.equals("mov") || 
+						fileFormat.equals("flv") || fileFormat.equals("mpeg") || 
+						fileFormat.equals("mkv") || fileFormat.equals("webm") || fileFormat.equals("3gp")) {
+					folderName = "VIDEO";
+				}
+				else {
+					folderName = "DOCUMENT";
+				}
+				File file = new File(tempFilePath + folderName + "/" + fileUpload);
+				if (file.exists()) {
+					File srcFile = new File(tempFilePath + folderName + "/" +  fileUpload);
+					File destFile = new File(actualFilePath + folderName + "/" + fileUpload);
+					try {
+						Files.copy(srcFile.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+						Files.delete(srcFile.toPath());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return ResponseEntity.ok().body(ktMaster);
+
 	}
-	}
-	}
-	}
-	return ResponseEntity.ok().body(ktMaster);
-	 
-	}
-	 
+
 	@GetMapping("/kt")
-	public ResponseEntity<List<Map<String,Object>>> getKTDetails(){
-	List<Map<String,Object>> ktDetails=ktMasterService.getKTDetails();
-	return ResponseEntity.ok().body(ktDetails);
-	 
-	 
+	public ResponseEntity<List<Map<String, Object>>> getKTDetails() {
+		List<Map<String, Object>> ktDetails = ktMasterService.getKTDetails();
+		return ResponseEntity.ok().body(ktDetails);
+
 	}
-	 
-	
-	 
-	@DeleteMapping("/deleteKT")
-	public ResponseEntity<Map<String,Object>> deleteKT(@RequestParam("ktId") Integer ktId){
-	 
-	    ktMasterService.deleteKT(ktId);
-	Map<String,Object> response=new HashMap<>();
-	response.put("status", 200);
-	response.put("deleted", "Employee is deleted successfuly");
-	return ResponseEntity.ok().body(response);
+
+	@DeleteMapping("/kt/{ktId}")
+	public ResponseEntity<Map<String, Object>> deleteKT(@PathVariable("ktId") Integer ktId) {
+
+		ktMasterService.deleteKT(ktId);
+		Map<String, Object> response = new HashMap<>();
+		response.put("status", 200);
+		response.put("deleted", "Employee is deleted successfuly");
+		return ResponseEntity.ok().body(response);
 	}
-	 
-	@GetMapping("/ktById/{ktId}")
-	public ResponseEntity<Map<String,Object>> getKTById(@PathVariable Integer ktId){
-	Map<String,Object> ktDetails=ktMasterService.getKTById(ktId);
-	return ResponseEntity.ok().body(ktDetails);
-	 
+
+	@GetMapping("/kt/{ktId}")
+	public ResponseEntity<Map<String, Object>> getKTById(@PathVariable("ktId") Integer ktId) {
+		Map<String, Object> ktDetails = ktMasterService.getKTById(ktId);
+		return ResponseEntity.ok().body(ktDetails);
+
 	}
-	 
-	 
-	
+
 }
